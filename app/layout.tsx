4235a716df9c8ce76a852de660f6c380b6ea94c2
Selimo Shano <mokherhi@gmail.com>
2024-10-19 14:17:11 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import Sidebar from "@/components/Sidebar";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "L Music-Basotho Pele",
+  description: "Mamela mino oa Basotho",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Sidebar>
+        {children}
+        </Sidebar>
+      </body>
+    </html>
+  );
+}
