132be8a04cfcf3f5fd97eb2e6481b9bb211d4fe8
Selimo Shano <mokherhi@gmail.com>
2024-10-28 17:10:45 +0200

    kokp update

---
+++ app/search/components/SearchContent.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import LikedButton from "@/components/LikedButton";
+import MediaItem from "@/components/MediaItem";
+import { Song } from "@/types";
+
+interface SearchContentProps {
+    songs: Song[];
+}
+
+const SearchContent: React.FC<SearchContentProps> = ({
+    songs
+}) => {
+    if (songs.length === 0) {
+        return (
+            <div
+            className="
+            flex
+            flex-col
+            gap-y-2
+            w-full
+            px-6
+            text-neutral-400
+            "
+            >
+                Pina e joalo ha e fumanehe!.
+            </div>
+        )
+    }
+    return(
+        <div className="flex flex-col gap-y-2 w-full px-6">
+            {songs.map((song) => (
+                <div 
+                key={song.id}
+                className="flex items-center gap-x-4 w-ful"
+                >
+                    <div className="flex-1">
+                        <MediaItem 
+                        onClick={() => {}}
+                        data={song}
+                        />
+                    </div>
+                    <LikedButton songId={song.id} />
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default SearchContent;
