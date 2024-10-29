f5b0ba3d532b9ec8eb57e9e02e61e03158c4da3b
Selimo Shano <mokherhi@gmail.com>
2024-10-29 17:06:46 +0200

    nolp

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -1,6 +1,7 @@
 import getLikedSongs from "@/actions/getLikedSongs";
 import Header from "@/components/Header";
 import Image from "next/image";
+import LikedContent from "./components/LikedContent";
 
 export const revalidate = 0;
 
@@ -35,11 +36,38 @@ const Liked = async () => {
                         lg:h-44
                         lg:w-44
                         ">
-                            <Image />
+                            <Image 
+                            fill
+                            alt="Playlist"
+                            className="object-cover"
+                            src="/images/liked.png"
+                            />
+                        </div>
+                        <div className="
+                        flex
+                        flex-col
+                        gap-y-2
+                        mt-4
+                        md:mt-0
+                        ">
+                            <p className="hidden md:block font-semibold text-sm">
+                                Playlist
+                            </p>
+                            <h1 
+                            className="
+                            text-white
+                            text-4xl
+                            sm:text-5xl
+                            lg:text-7xl
+                            font-bold
+                            ">
+                                Liked Songs
+                            </h1>
                         </div>
                     </div>
                 </div>
             </Header>
+            <LikedContent songs={songs} />
         </div>
     )
 }
