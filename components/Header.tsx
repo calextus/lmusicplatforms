e667642808ca2acafcf535ae08b9770a83f7a62a
Selimo Shano <mokherhi@gmail.com>
2024-11-13 12:13:23 +0200

    ini

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useRouter } from "next/navigation";
 import { BiSearch } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
@@ -27,45 +25,50 @@ const Header: React.FC<HeaderProps> = ({
     const supabaseClient = useSupabaseClient();
     const { user } = useUser();
 
-
     const handleLogout = async () => {
         const { error } = await supabaseClient.auth.signOut();
-        //Reset any playing songs in the future
+        // Reset any playing songs in the future
         router.refresh();
 
         if (error) {
             toast.error(error.message);
-        }else{
-            toast.success('Logged out!')
+        } else {
+            toast.success("Logged out!");
         }
-    }
+    };
+
     return (
         <div
-        className={twMerge (`
+            className={twMerge(
+                `
             h-fit
             bg-gradient-to-b
             from-blue-600
             p-6
             `,
-        className
-        )}
+                className
+            )}
         >
-            <div className="
+            <div
+                className="
             w-full
             mb-4
             flex
             items-center
             justify-between
-            ">
-                <div className="
+            "
+            >
+                <div
+                    className="
                 hidden
                 md:flex
                 gap-x-2
                 items-center
-                ">
+                "
+                >
                     <button
-                    onClick={() => router.back()}
-                    className="
+                        onClick={() => router.back()}
+                        className="
                     rounded-full
                     bg-black
                     flex
@@ -79,8 +82,8 @@ const Header: React.FC<HeaderProps> = ({
                     </button>
 
                     <button
-                    onClick={() => router.forward()}
-                    className="
+                        onClick={() => router.forward()}
+                        className="
                     rounded-full
                     bg-black
                     flex
@@ -92,12 +95,13 @@ const Header: React.FC<HeaderProps> = ({
                     >
                         <RxCaretRight className="text-white" size={35} />
                     </button>
-
                 </div>
 
+                {/* Mobile buttons for Home and Search */}
                 <div className="flex md:hidden gap-x-2 items-center">
                     <button
-                    className="
+                        onClick={() => router.push("/")}
+                        className="
                     rounded-full
                     p-2
                     bg-white
@@ -107,11 +111,12 @@ const Header: React.FC<HeaderProps> = ({
                     transition
                     "
                     >
-                        <HiHome className="text-black" size={20}/>
+                        <HiHome className="text-black" size={20} />
                     </button>
 
                     <button
-                    className="
+                        onClick={() => router.push("/search")}
+                        className="
                     rounded-full
                     p-2
                     bg-white
@@ -121,14 +126,12 @@ const Header: React.FC<HeaderProps> = ({
                     transition
                     "
                     >
-                        <BiSearch className="text-black" size={20}/>
+                        <BiSearch className="text-black" size={20} />
                     </button>
-
-
                 </div>
 
-                <div 
-                className="
+                <div
+                    className="
                 flex
                 justify-between
                 items-center
@@ -137,61 +140,54 @@ const Header: React.FC<HeaderProps> = ({
                 >
                     {user ? (
                         <div className="flex gap-x-4 items-center">
-
                             <Button
-                            onClick={handleLogout}
-                            className="bg-white px-6 py-2"
+                                onClick={handleLogout}
+                                className="bg-white px-6 py-2"
                             >
                                 Logout
                             </Button>
 
-                            <Button 
-                            onClick={() => router.push('/account')}
-                            className="bg-white"
+                            <Button
+                                onClick={() => router.push("/account")}
+                                className="bg-white"
                             >
                                 <FaUserAlt />
                             </Button>
-
                         </div>
                     ) : (
-                    
-                    <>
-                    <div>
-                        <Button 
-                        onClick={authModal.onOpen}
-                        className="
-                        bg-transparent
-                        text-neutral-300
-                        font-medium
-                        "
-                        >
-                            Sign Up
-                        </Button>
-                        
-                    </div>
+                        <>
+                            <div>
+                                <Button
+                                    onClick={authModal.onOpen}
+                                    className="
+                                    bg-transparent
+                                    text-neutral-300
+                                    font-medium
+                                    "
+                                >
+                                    Sign Up
+                                </Button>
+                            </div>
 
-                    <div>
-                        <Button 
-                        onClick={authModal.onOpen}
-                        className="
-                        bg-white
-                        px-6
-                        py-2
-                        "
-                        >
-                            Log In
-                        </Button>
-                        
-                    </div>
-                    </>
+                            <div>
+                                <Button
+                                    onClick={authModal.onOpen}
+                                    className="
+                                    bg-white
+                                    px-6
+                                    py-2
+                                    "
+                                >
+                                    Log In
+                                </Button>
+                            </div>
+                        </>
                     )}
-
-
                 </div>
             </div>
             {children}
         </div>
-    )
-}
+    );
+};
 
 export default Header;
